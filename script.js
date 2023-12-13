3f9dd7212e223837b030c7bda4a9abbee55a6c47
Halfonx <jermy.prigent@gmail.com>
2023-12-13 22:51:50 +0100

    Add somme CSS to the body and the div and change variable name in JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 
-const createDiv = function(numberOfDiv) {
-    for (let i = 0; i <= numberOfDiv; i++) {
+const createRow = function(numberOfDiv) {
+    for (let i = 0; i < numberOfDiv; i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('grid-element');
         container.appendChild(newDiv);
