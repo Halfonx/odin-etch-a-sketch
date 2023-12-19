d1bf309a61685389ede20f641b0c2ac650818005
Halfonx <jermy.prigent@gmail.com>
2023-12-19 23:06:13 +0100

    Create the grid with fixed values in the code

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 
-const createRow = function(numberOfDiv) {
-    for (let i = 0; i < numberOfDiv; i++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('grid-element');
-        container.appendChild(newDiv);
+const createGrid = function() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < 16; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+    container.appendChild(row);
     }
 }
